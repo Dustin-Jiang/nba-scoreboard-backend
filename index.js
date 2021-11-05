4ba6a33e38c2b3f5192df4cdaf32a30fe27bbb24
Dustin Jiang <dustinjiang@outlook.com>
2021-11-05 09:21:32 +0800

    [Content] Add basic code for status code

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const https = require("https")
 const express = require("express")
 var app = express()
 
-app.get("/", function(req, res) {
-    res.send("NBA-Scoreboard-Backend Home")
+app.get("/", function (req, res) {
+  res.send("NBA-Scoreboard-Backend Home")
 });
 
-app.get("/score", function(req, res) {
-    
+app.get("/score/today", function (req, res) {
+  https.get("https://china.nba.com/static/data/scores/miniscoreboard.json", (result) => {
+    if (result.statusCode == 304) {
+      //Not Modified
+      res.statusCode(304).send()
+    }
+    if (result.statusCode == 200) {
+      res.send(result)
+    }
+  })
 })
