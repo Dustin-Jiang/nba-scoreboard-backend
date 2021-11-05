3fd57745320f746ce0f90c3e7971048c7682e908
Dustin Jiang <dustinjiang@outlook.com>
2021-11-05 23:33:48 +0800

    [Config] Enable CORS

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ var dataStorage = require("./dataStorage")
 var modifier = require("./modifier")
 
 app.get("/", function (req, res) {
+  //Enable CORS
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*')
   res.send("NBA-Scoreboard-Backend Home")
 });
 
 app.get("/score/today", function (req, res) {
+  //Enable CORS
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*')
+
   dataStorage.get()
   .then((result) => {
     res.send(modifier.today(result))
